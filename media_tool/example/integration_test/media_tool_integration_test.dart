a26cae598b1194fcb358fd079bf9d0924ddcc079
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-06-02 22:15:10 +0300

    Update tests

--- media_tool/example/integration_test/media_tool_integration_test.dart
+++ media_tool/example/integration_test/media_tool_integration_test.dart
@@ -7,8 +7,7 @@ void main() {
 
   group('MediaTool', () {
     group('getPlatformName', () {
-      test('returns correct name when platform implementation exists',
-          () async {
+      test('returns correct name when platform implementation exists', () async {
         String platformName;
         if (Platform.isIOS) {
           platformName = 'iOS';
@@ -20,6 +19,24 @@ void main() {
 
         final actualPlatformName = await getPlatformName();
         expect(actualPlatformName, equals(platformName));
+      });
+    });
+
+    group('Video Compression', () {
+      test('simple', () async {
+        const options = VideoCompressOptions(
+          id: '10001',
+          path: '/Users/starkdmi/Downloads/oludeniz.MOV',
+          destination: '/Users/starkdmi/Downloads/oludeniz.compressed.mov',
+        );
+        
+        final task = VideoTool.compress(options);
+        /*await for (final event in task.events) {
+          print(event);
+        }*/
+        expect(task.events, emitsThrough(
+          const VideoCompressCompletedEvent(url: 'file:///Users/starkdmi/Downloads/oludeniz.compressed.mov'),
+        ),);
       });
     });
   });
