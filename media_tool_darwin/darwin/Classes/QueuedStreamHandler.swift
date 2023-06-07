a13d266855ae8306a213b80902add41995a97c0b
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-06-08 00:07:52 +0300

    Update QueuedStreamHandler.swift
    Fix multiple instances use same DispatchQueue for read/write sync

--- media_tool_darwin/darwin/Classes/QueuedStreamHandler.swift
+++ media_tool_darwin/darwin/Classes/QueuedStreamHandler.swift
@@ -6,16 +6,17 @@ import FlutterMacOS
 
 /// Implementation of `FlutterStreamHandler` which queue messages while not subscribed to
 public class QueuedStreamHandler: NSObject, FlutterStreamHandler {
-    var eventChannel: FlutterEventChannel?
+    private var eventChannel: FlutterEventChannel!
     private var eventSink: FlutterEventSink?
 
-    private let accessQueue = DispatchQueue(label: "com.starkdev.media.tool.sync.queue")
+    private var accessQueue: DispatchQueue!
     private var queue = [Any]()
 
     public init(name: String, messenger: FlutterBinaryMessenger) {
         super.init()
         eventChannel = FlutterEventChannel(name: name, binaryMessenger: messenger)
-        eventChannel!.setStreamHandler(self)
+        eventChannel.setStreamHandler(self)
+        accessQueue = DispatchQueue(label: "\(name).QueuedStreamHandler.accessQueue")
     }
 
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
