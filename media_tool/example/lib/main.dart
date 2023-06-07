dfd6f31217e49edf5b3e36159119b5f69231100e
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-06-07 23:39:01 +0300

    Implement simple example

--- media_tool/example/lib/main.dart
+++ media_tool/example/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:media_tool/media_tool.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(const MyApp());
 
@@ -20,7 +24,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String? _platformName;
+  final _uuid = const Uuid();
+  VideoCompressionTask? _task;
+  StreamSubscription<VideoCompressEvent>? _subscription;
+  VideoCompressEvent? _event;
+
+  @override void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,30 +42,73 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (_platformName == null)
+            if (_event == null)
               const SizedBox.shrink()
             else
               Text(
-                'Platform Name: $_platformName',
+                _event.toString(),
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
             const SizedBox(height: 16),
+
             ElevatedButton(
               onPressed: () async {
-                try {
-                  /*final result = await getPlatformName();
-                  setState(() => _platformName = result);*/
-                } catch (error) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor: Theme.of(context).primaryColor,
-                      content: Text('$error'),
-                    ),
-                  );
-                }
+                setState(() {
+                  _task = null;
+                  _event = null;
+                });
+
+                final result = await FilePicker.platform.pickFiles();
+                final path = result?.files.first.path;
+                if (path == null) return;
+                debugPrint(path);
+
+                final temp = await getTemporaryDirectory();
+                final destination = '${temp.path}/media_tool_compressed.mov';
+                debugPrint(destination);
+
+                final task = VideoTool.compress(
+                  id: _uuid.v4(), 
+                  path: path, 
+                  destination: destination, 
+                  videoSettings: const VideoSettings(
+                    codec: VideoCodec.h265,
+                    bitrate: 2000000,
+                    // quality: 0.75,
+                    size: Size(1280.0, 1280.0),
+                  ),
+                  skipAudio: true,
+                  // audioSettings: const AudioSettings(codec: AudioCodec.opus, bitrate: 96000, sampleRate: 44100),
+                  overwrite: true,
+                );
+
+                _subscription = task.events.listen((event) { 
+                  setState(() => _event = event);
+
+                  //   ScaffoldMessenger.of(context).showSnackBar(
+                  //     SnackBar(
+                  //       backgroundColor: Theme.of(context).primaryColor,
+                  //       content: Text('$error'),
+                  //     ),
+                  //   );
+                });
+
+                setState(() => _task = task);
               },
-              child: const Text('Get Platform Name'),
+              child: const Text('Select video'),
             ),
+
+            if (_task != null)
+              ElevatedButton(
+                onPressed: () async {
+                  final cancelled = await _task?.cancel();
+                  if (cancelled != true) { 
+                    return; 
+                  }
+                  setState(() => _task = null);
+                },
+                child: const Text('Cancel'),
+              ),
           ],
         ),
       ),
