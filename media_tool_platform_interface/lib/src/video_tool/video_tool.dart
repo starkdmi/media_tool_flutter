4e0a9e3513b162776f5c34aa136a504a5902d150
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-10-23 22:56:02 +0300

    Update video_tool.dart

--- media_tool_platform_interface/lib/src/video_tool/video_tool.dart
+++ media_tool_platform_interface/lib/src/video_tool/video_tool.dart
@@ -1,3 +1,3 @@
-export 'package:media_tool_platform_interface/src/video_tool/video_codec.dart';
 export 'package:media_tool_platform_interface/src/compression_event.dart';
+export 'package:media_tool_platform_interface/src/video_tool/video_codec.dart';
 export 'package:media_tool_platform_interface/src/video_tool/video_settings.dart';
